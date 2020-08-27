cde03b00b738a2f9030b836326689d05239759bf
pena-daniel <63110462+pena-daniel@users.noreply.github.com>
2020-08-27 10:01:28 +0200

    debut de la page de presentation des cours

--- js/contactScript.js
+++ js/contactScript.js
@@ -6,7 +6,6 @@ let visible = document.querySelector(".deplacement span");
 var image_1 = document.getElementById("image_1");
 let titleForgroup = document.querySelector(".titleForgroups");
 let innerTextForgroup = document.querySelector(".innerTextForgroup");
-let menu =document.querySelector('.navMenu');
 var cursor = 0;
 
 
@@ -51,28 +50,3 @@ function setBackVisible() {
   console.log(position);
 }
 
-// color du menu au scrool
-
-window.addEventListener("scroll",(e)=>{
-  menu.style.backgroundColor = 'rgb(17, 16, 16)';
-});
-
-
-// gestion du menu responsive
-
-let button = document.querySelector(".subMenu");
-let submenu = document.querySelector(".responsive");
-let reviens = document.querySelector(".back-to");
-let c = 0;
-
-button.addEventListener("click", setPosition);
-reviens.addEventListener("click", setPosition);
-function setPosition() {
-  if (c == 0) {
-    submenu.style.left = " 0%";
-    c = 1;
-  } else {
-    submenu.style.left = " -100%";
-    c = 0;
-  }
-}
--- js/menu.js
+++ js/menu.js
@@ -5,18 +5,18 @@ let menu =document.querySelector('.navMenu');
 // color du menu au scrool
 
 window.addEventListener("scroll",(e)=>{
-  menu.style.backgroundColor = 'rgb(17, 16, 16)';
+  menu.style.backgroundColor = 'rgba(240, 240, 221, 0.979)';
 });
 
 
 // gestion du menu responsive
 
-let button = document.querySelector(".subMenu");
+let buttons = document.querySelector(".subMenu");
 let submenu = document.querySelector(".responsive");
 let reviens = document.querySelector(".back-to");
 let c = 0;
 
-button.addEventListener("click", setPosition);
+buttons.addEventListener("click", setPosition);
 reviens.addEventListener("click", setPosition);
 function setPosition() {
   if (c == 0) {
